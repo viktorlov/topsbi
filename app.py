e55e8af95c2c3222dafb1fd78126399b21ccd303
viktorlov <orlovva@ymail.com>
2024-08-01 22:25:58 +0300

    (va0r) update app.py

--- app.py
+++ app.py
@@ -1,27 +1,49 @@
-import pandas as pd
+import subprocess
 
-from FixedVariables.constants import Classes__dict, plm__dict, plmHierarchy__dict
+from dotenv import load_dotenv, find_dotenv
+from flask import Flask, render_template
+
+from DataLoaderModule.code import load_data
 from LoguruModule.code import LoguruDecoratorClass
 from PostgresDBModule.code import PostgresDBClass
 
+app = Flask(__name__)
 
-class ApplicationClass:
+load_dotenv(find_dotenv())
 
-    @LoguruDecoratorClass(level="INFO")
-    def load_data(self):
 
-        db = PostgresDBClass()
+@LoguruDecoratorClass(level="INFO")
+def get_db_connection():
+    db = PostgresDBClass()
+    return db
 
-        Classes = pd.read_csv('./CSVData/Classes.csv')
-        db.load_dataframe(Classes, 'Classes', dtype=Classes__dict)
 
-        plm = pd.read_csv('./CSVData/plm.csv')
-        db.load_dataframe(plm, 'plm', dtype=plm__dict)
+@app.route('/')
+def index():
+    db = get_db_connection()
+    table_exists = db.check_table_exists('joined')
 
-        plmHierarchy = pd.read_csv('./CSVData/plmHierarchy.csv')
-        db.load_dataframe(plmHierarchy, 'plmHierarchy', dtype=plmHierarchy__dict)
+    if table_exists:
+        query = f"""
+        SELECT "Name_plm", "oboznachenie_421975", "Name_Classes" 
+        FROM joined
+        WHERE "oboznachenie_421975" IS NOT NULL
+        """
+        items = db.execute_query(query)
+        db.close()
+        return render_template('index.html', items=items)
+    else:
+        db.close()
+        return "Table 'joined' does not exist."
+
+
+@LoguruDecoratorClass(level="INFO")
+def start_flask_app():
+    subprocess.run(["gunicorn", "-w", "4", "app:app"])
 
 
 if __name__ == "__main__":
-    app = ApplicationClass()
-    app.load_data()
+    if load_data():
+        start_flask_app()
+    else:
+        print("Data loading failed. The application will not start.")
