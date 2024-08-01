6d4dd70cadd4f5cc75ffc3f759efb281d84bff9a
viktorlov <orlovva@ymail.com>
2024-08-01 22:25:49 +0300

    (va0r) update PostgresDBModule

--- PostgresDBModule/code.py
+++ PostgresDBModule/code.py
@@ -1,7 +1,8 @@
 import os
 
+import psycopg2
 from dotenv import load_dotenv, find_dotenv
-from sqlalchemy import create_engine, MetaData
+from sqlalchemy import create_engine
 
 from LoguruModule.code import LoguruDecoratorClass
 
@@ -9,20 +10,44 @@ load_dotenv(find_dotenv())
 
 
 class PostgresDBClass:
-    """
-    Класс для работы с базой данных PostgreSQL.
-    Использует SQL Alchemy для записи исторических и текущих данных криптовалют в БД.
-    """
-
     def __init__(self):
         postgres_access_line = os.getenv('POSTGRES_ACCESS_LINE')
         if not postgres_access_line:
             raise ValueError("POSTGRES_ACCESS_LINE environment variable is not set")
         self.postgres_url = f"postgresql://{postgres_access_line}"
         self.engine = create_engine(self.postgres_url)
-        self.metadata = MetaData()
-        self.metadata.reflect(bind=self.engine)
+
+        load_dotenv(find_dotenv())
+        self.conn = psycopg2.connect(
+            host=os.getenv('HOST'),
+            database=os.getenv('DATABASE'),
+            user='postgres',
+            password=os.getenv('PASSWORD')
+        )
 
     @LoguruDecoratorClass(level="INFO")
     def load_dataframe(self, dataframe, table_name, if_exists='replace', index=False, dtype=None):
         dataframe.to_sql(table_name, self.engine, if_exists=if_exists, index=index, dtype=dtype)
+
+    @LoguruDecoratorClass(level="INFO")
+    def execute_query(self, query):
+        cursor = self.conn.cursor()
+        cursor.execute(query)
+        result = cursor.fetchall()
+        cursor.close()
+        return result
+
+    @LoguruDecoratorClass(level="INFO")
+    def close(self):
+        self.conn.close()
+
+    @LoguruDecoratorClass(level="INFO")
+    def check_table_exists(self, table_name):
+        query = f"""
+        SELECT EXISTS (
+            SELECT FROM information_schema.tables 
+            WHERE table_name = '{table_name}'
+        );
+        """
+        result = self.execute_query(query)
+        return result[0][0]
