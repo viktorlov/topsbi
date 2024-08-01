60f3b369bcd42dbc8a7ef81fa14c357e66c47f25
viktorlov <orlovva@ymail.com>
2024-08-01 12:30:55 +0300

    (va0r) update app.py (code reformat)

--- app.py
+++ app.py
@@ -9,12 +9,16 @@ class ApplicationClass:
 
     @LoguruDecoratorClass(level="INFO")
     def load_data(self):
+
         db = PostgresDBClass()
+
         Classes = pd.read_csv('./CSVData/Classes.csv')
-        plm = pd.read_csv('./CSVData/plm.csv')
-        plmHierarchy = pd.read_csv('./CSVData/plmHierarchy.csv')
         db.load_dataframe(Classes, 'Classes', dtype=Classes__dict)
+
+        plm = pd.read_csv('./CSVData/plm.csv')
         db.load_dataframe(plm, 'plm', dtype=plm__dict)
+
+        plmHierarchy = pd.read_csv('./CSVData/plmHierarchy.csv')
         db.load_dataframe(plmHierarchy, 'plmHierarchy', dtype=plmHierarchy__dict)
 
 
