c9a1a019d6a72b0a7f72dbd8622a2865d8605489
viktorlov <orlovva@ymail.com>
2024-08-01 12:28:27 +0300

    (va0r) initial app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+from FixedVariables.constants import Classes__dict, plm__dict, plmHierarchy__dict
+from LoguruModule.code import LoguruDecoratorClass
+from PostgresDBModule.code import PostgresDBClass
+
+
+class ApplicationClass:
+
+    @LoguruDecoratorClass(level="INFO")
+    def load_data(self):
+        db = PostgresDBClass()
+        Classes = pd.read_csv('./CSVData/Classes.csv')
+        plm = pd.read_csv('./CSVData/plm.csv')
+        plmHierarchy = pd.read_csv('./CSVData/plmHierarchy.csv')
+        db.load_dataframe(Classes, 'Classes', dtype=Classes__dict)
+        db.load_dataframe(plm, 'plm', dtype=plm__dict)
+        db.load_dataframe(plmHierarchy, 'plmHierarchy', dtype=plmHierarchy__dict)
+
+
+if __name__ == "__main__":
+    app = ApplicationClass()
+    app.load_data()
